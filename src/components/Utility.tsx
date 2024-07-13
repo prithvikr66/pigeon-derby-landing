e985a198961ce4d6f90506918cc0086aa901e592
prithvikr66 <pkunofficial66@gmail.com>
2024-07-14 01:51:28 +0530

    utility part

--- src/components/Utility.tsx
+++ src/components/Utility.tsx
@@ -1,23 +1,67 @@
 import Utility1Image from "../assets/images/Utility1.png";
-import Utility2Image from "../assets/images/Utility2.png"
-import Utility3Image from "../assets/images/Utility3.png"
+import Utility2Image from "../assets/images/Utility2.png";
+import Utility3Image from "../assets/images/Utility3.png";
+import Utility4Image from "../assets/images/Utility4.png";
 const Utility = () => {
   return (
     <div>
       <div>
-        <h3 className=" font-paladins-straight  text-[40px] text-[#F01E31]">
+        <h3 className=" font-paladins-straight  text-[40px] text-[#F01E31] text-center lg:text-left">
           Utility
         </h3>
 
-        <div className=" w-[80%] mx-auto border border-white">
-          <div className=" font-advent-regular text-[48px] lg:text-[54px] text-white w-[25%]">
+        <div className=" w-[80%] mx-auto  flex flex-col lg:flex-row justify-between gap-[40px] lg:gap-0">
+          <div className=" font-advent-regular text-[30px] lg:text-[54px] text-white w-[80%] mx-auto lg:mx-0 lg:w-[25%] text-center lg:text-left ">
             what do they call a token with utility?
-            <p className=" text-[#F01E31] mt-[-20px]">Kocher!</p>
-            <img src={Utility1Image} />
+            <p className=" text-[#F01E31]  text-center lg:text-left ">
+              Kocher!
+            </p>
+            <img src={Utility1Image} className="  hidden lg:block" />
           </div>
 
-          <div>
-            <img src={}/>
+          <div className=" w-[80%] mx-auto lg:mx-0 lg:w-[18%]">
+            <img src={Utility2Image} className="sm:mx-auto lg:mx-0 " />
+            <p className=" uppercase text-[#FFFF00] font-advent-bold text-[24px] sm:text-center lg:text-left ">
+              race
+            </p>
+            <p className=" font-advent-semibold uppercase text-white text-[20px] sm:text-center lg:text-left">
+              a unique lottery game where{" "}
+              <span className=" text-[#F01E31]">
+                50% of the players win a 2x
+              </span>{" "}
+              compared to justa 1 person taking the whOle pot!
+            </p>
+          </div>
+          <div className=" w-[80%] mx-auto lg:mx-0 lg:w-[18%] ">
+            <img
+              src={Utility3Image}
+              className="sm:mx-auto lg:mx-0 lg:hidden "
+            />
+
+            <p className=" uppercase text-[#FFFF00] font-advent-bold text-[24px] sm:text-center lg:text-left ">
+              Love shack
+            </p>
+            <p className=" font-advent-semibold uppercase text-white text-[20px] sm:text-center lg:text-left">
+              <span className=" text-[#F01E31]">Stake $Race</span> and be
+              eligible for passive income. we make revenue by taking a 1% fee on
+              every lottery ticket sold..
+            </p>
+            <img
+              src={Utility3Image}
+              className="sm:mx-auto lg:mx-0  hidden lg:block"
+            />
+          </div>
+
+          <div className=" w-[80%] mx-auto lg:mx-0 lg:w-[18%]">
+            <img src={Utility4Image} className="sm:mx-auto lg:mx-0 " />
+            <p className=" uppercase text-[#FFFF00] font-advent-bold text-[24px]  sm:text-center lg:text-left">
+              Burn
+            </p>
+            <p className=" font-advent-semibold uppercase text-white text-[20px] sm:text-center lg:text-left">
+              With each lottery ticket you buy, a small portion of{" "}
+              <span className="  text-[#F01E31]">$RACE is burnt!</span>{" "}
+              deflationary, you know...
+            </p>
           </div>
         </div>
       </div>
